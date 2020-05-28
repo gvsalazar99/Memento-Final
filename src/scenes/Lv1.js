5279f9d705aa6f3e10ed1352fbfc294049802e47
gvsalazar99 <63384402+gvsalazar99@users.noreply.github.com>
2020-05-27 20:45:05 -0700

    progress bar + level 2 items + fixed hit boxes

--- src/scenes/Lv1.js
+++ src/scenes/Lv1.js
@@ -5,20 +5,25 @@ class Lv1 extends Phaser.Scene {
 
 	
 	create() {
+
 		//camera fade in n out 
 		this.cameras.main.once('camerafadeoutcomplete', function (camera) {
-			this.add.image(1260, 590, 'levelonenew').setOrigin(0, 0);
+			this.add.image(1260, 590, 'levelonenew').setOrigin(0, 0).setScale(.4,.4);;
 			camera.fadeIn(300, 0,0,0);
 
 		 //create sounds
 		 this.music = this.sound.add('level1music');
-		 //this.music.play( {loop:true} );
+		 this.music.play( {loop:true} );
 		 if(mute == false) { this.music.play( { loop: true} ); } //play if unmuted
 
 
 
 		//create background
 		let background = this.add.sprite(0,0, 'levelonenew').setOrigin(0, 0);
+
+		//progressbar
+		let progressbar = this.add.sprite('progressbar');
+		progressbar= this.add.sprite(game.config.width/3.58,0, 'progressbar').setOrigin(0, 0).setScale(.35,.32);
 		
 		//exit button switches scenes to level 2    
 		this.tempLevl2button = this.add.sprite(game.config.width/4,game.config.height/4, 'exitbutton').setScale(0.25,0.25).setOrigin(0);
@@ -26,7 +31,7 @@ class Lv1 extends Phaser.Scene {
 		this.tempLevl2button.on('pointerdown',()=> {this.scene.start('Level2'); this.music.stop()});
 
 		//dialogue box art
-		this.dialogueBox = this.add.sprite(game.config.width/4.5, 0, 'blue1').setOrigin(0).setScale(.55,.4);
+		this.dialogueBox = this.add.sprite(game.config.width/4.5, 0, 'level1box').setOrigin(0).setScale(.4,.4);
 		this.dialogueBox.y = game.config.height/1.01 - this.dialogueBox.displayHeight;
 
 		//create continue text prompt
@@ -44,11 +49,7 @@ class Lv1 extends Phaser.Scene {
 			}
 		});
 
-		//text
-		boxText = this.add.text(410, this.dialogueBox.y + 15, '', { font: "15pt Courier", fill: "#ff0000", stroke: "#ff0000", strokeThickness: 1, wordWrap: { width: 570, useAdvancedWrap: true } });
-		//boxText.setText('Welcome to level 1! Click on the magnolia for instructions!');
-		boxText.visible = true;
-		typeText(this, 'Welcome to level 1! Take a look around and click on what piques your interest');
+		
 		
 
 		//clickable memento magnolia
@@ -58,8 +59,9 @@ class Lv1 extends Phaser.Scene {
 							'.... what, do you like me or something? Wtf are you looking at?',
 							'...'];
 		this.magnolia.makeInteractive();
+
 		//tree carving
-		this.carving = new memento(this, game.config.width*.0000000000001, game.config.height*.069, 'treecarving').setOrigin(0).setScale(.75);
+		this.carving = new memento(this, 10, 200, 'treecarving').setOrigin(0).setScale(.3);
 		this.carving.text = ['X+A, a romantic display of affection',
 			'A contract ripped into the bark as a demonstration',
 			'After the guilty often return to the crime scene',
@@ -67,12 +69,19 @@ class Lv1 extends Phaser.Scene {
 			'Sometimes we hurt each other and lose a shared, beloved sentiment'];
 		this.carving.makeInteractive();
 		//squirrel
-		this.squirrel = new memento(this, game.config.width*.455, game.config.height*.49, 'squirrel').setOrigin(0).setScale(.6);
+		this.squirrel = new memento(this, 638, 340, 'squirrel').setOrigin(0).setScale(.64);
 		this.squirrel.text = ['[Squirrel sounds]', 
 							'...',
 							'[Squirrel sounds intensify]',
 							'...'];
 		this.squirrel.makeInteractive();
+
+		//text
+		boxText = this.add.text(430, this.dialogueBox.y + 15, '', { font: "14pt Courier", fill: "#000000", stroke: "#000000", wordWrap: { width: 570, useAdvancedWrap: true } });
+		//boxText.setText('Welcome to level 1! Click on the magnolia for instructions!');
+		boxText.visible = true;
+		typeText(this, 'Welcome to level 1! Take a look around and click on what piques your interest');
+		
 		//butterfly
 		this.butterfly = new memento(this, game.config.width*.0455, game.config.height*.5, 'butterflysmall').setOrigin(0).setScale(.8);
 		this.butterfly.text = ['[oink, oink]',
--- src/scenes/Menu.js
+++ src/scenes/Menu.js
@@ -18,39 +18,41 @@ class Menu extends Phaser.Scene {
       this.load.image('purple', './assets/wallpaper.png'); 
       this.load.image('credits', './assets/EndScreen (1).PNG'); 
       //dialogue boxes
-      this.load.image('blue1', './assets/TextBoxes/BlueLevel1.PNG');
-      this.load.image('blue2', './assets/TextBoxes/BlueLevel2.PNG');
-      this.load.image('blue3', './assets/TextBoxes/BlueLevel3.PNG');
-      this.load.image('yellow1', './assets/TextBoxes/YellowLevel1.PNG');
-      this.load.image('yellow2', './assets/TextBoxes/YellowLevel2.PNG');
-      this.load.image('yellow3', './assets/TextBoxes/YellowLevel3.PNG');
+      this.load.image('level2box', './assets/LevelTwoTextBox.PNG');
+      this.load.image('level1box', './assets/LevelOneTextBox.PNG');
+      this.load.image('level3box', './assets/LevelThreeTextBox.PNG');
       this.load.image('mementomenu', './assets/MenuScreen.PNG');
       //level backgrounds
       this.load.image('levelone', './assets/LevelOneBackground.PNG');
       this.load.image('levelonenew', './assets/LevelOneBackgroundResized.png');
+      this.load.image('leveltwo', './assets/LevelTwoBackground.png');
 
       //sounds
       this.load.audio('level1music', 'assets/bensound-tenderness.mp3');
       this.load.audio('level2music', 'assets/bensound-beyondtheline.mp3');
       this.load.audio('level3music', 'assets/bensound-sweet.mp3');
 
+      //progress bar
+      this.load.image('progressbar', './assets/ballAndBar.PNG');
+
+
 
 
 
 
       //mementos for level1
-      this.load.image('treecarving', './assets/treecarving.PNG');
-      this.load.image('squirrel', './assets/squirrel.PNG');
+      this.load.image('treecarving', './assets/treecarvingsmall.png');
+      this.load.image('squirrel', './assets/squirrelsmall.png');
       this.load.image('butterflysmall', './assets/butterflysmall.png');
       this.load.image('magnolianew', './assets/magnolianoglow.PNG');
       this.load.image('girlsmall', './assets/girlsmall.png');
 
        //mementos for level2 (temp)
-       this.load.image('seaweedtemp', './assets/seaweedtemp.png');
-       this.load.image('clamtemp', './assets/clamtemp.png');
-       this.load.image('crabtemp', './assets/crabtemp.png');
-       this.load.image('seahorse', './assets/seahorse.png');
-       this.load.image('seaweedtemp', './assets/seaweedtemp.png');
+       this.load.image('seahorses', './assets/twoseahorses.png');
+       this.load.image('seaweed', './assets/seaweed.png');
+       this.load.image('shell', './assets/shell.png');
+       this.load.image('starfish', './assets/starfish.png');
+       this.load.image('fish', './assets/fishsmall.png');
 
        //mementos for level3 (temp)
        this.load.image('signtemp', './assets/signtemp.png');
@@ -74,22 +76,24 @@ class Menu extends Phaser.Scene {
 
       //button interactivity in menu 
       background = this.add.sprite(0, 0, 'mementomenu').setOrigin(0, 0);
-    
-
-
+  
       //background.x = game.config.height - background.width;
 
       this.button = this.add.sprite(game.config.width/2,game.config.height/2, 'playbutton').setScale(0.3,0.3).setOrigin(0);
       this.button.setInteractive();
       this.button.on('pointerdown',()=> this.scene.start('Level1'))
 
+
+
+
       }, this);
   
       this.cameras.main.fadeOut(300, 0,0,0);
     
-      
-      
-        
+  
+
+
+  
 
     }
 
